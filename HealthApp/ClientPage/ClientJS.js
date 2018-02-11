04eba298a24d1ba5597071fb2ad9fa66c959d7b6
smrahman129 <smrahman@ucsd.edu>
2018-02-10 18:46:54 -0800

    Again VS trying to push

---
+++ HealthApp/ClientPage/ClientJS.js
@@ -0,0 +1,17 @@
+﻿function changeP() {
+    document.getElementById("macros").innerHTML = "HAHA"
+}
+
+function send_message() {
+    message = document.getElementById("chat_input").value;
+    add2live(message);
+    document.getElementById("chat_input").value = "";
+}
+
+function add2live(message) {
+    var table = document.getElementById("live_table");
+
+    var newRow = table.insertRow(-1);
+    var cell = newRow.insertCell(0);
+    cell.innerHTML = message;
+}
