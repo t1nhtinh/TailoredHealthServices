06f3a39ecb77b555e07f3562d0c13ecd75e85bca
smrahman129 <smrahman@ucsd.edu>
2018-02-17 15:31:36 -0800

    Working chat but no css differentiation

--- HealthApp/ClientPage/ClientJS.js
+++ HealthApp/ClientPage/ClientJS.js
@@ -1,17 +1,26 @@
 ﻿function changeP() {
-    document.getElementById("macros").innerHTML = "HAHA"
+    if(typeof(Storage) != "undefined") {
+        cacheTable("week_goal_table");
+        document.getElementById("macros").innerHTML = localStorage.getItem("hello");
+        var tableArr = JSON.parse(localStorage.getItem("week_goal_table"));
+        for (i = 0; i < tableArr.length; i++) {
+            console.log(tableArr[i]);
+        }
+
+        }
+    else {
+    }
 }
 
-function send_message() {
-    message = document.getElementById("chat_input").value;
-    add2live(message);
-    document.getElementById("chat_input").value = "";
+function cacheTable (table_id) {
+    var tableHandle = document.getElementById(table_id);
+    var tableArr = new Array();
+    for (i = 0; i < tableHandle.rows.length; i++) {
+        tableArr[tableArr.length] = tableHandle.rows[i].cells[0].innerHTML;
+    }
+    for (i = 0; i < tableArr.length; i++) {
+        console.log(tableArr[i]);
+    }
+    localStorage.setItem(table_id, JSON.stringify(tableArr));
 }
 
-function add2live(message) {
-    var table = document.getElementById("live_table");
-
-    var newRow = table.insertRow(-1);
-    var cell = newRow.insertCell(0);
-    cell.innerHTML = message;
-}
---
+++ HealthApp/Messenger/messenger.js
@@ -0,0 +1,93 @@
+﻿var toWho = "";
+var whoDis = "";
+var message_p1= "";
+var message_p2= "";
+
+setFirstWhos(location);
+displayTable();
+function displayTable() {
+    determineTable(whoDis, toWho);
+    var curTable = localStorage.getItem(message_p1+ "/" + message_p2);
+    var tableHandle = document.getElementById("live_table");
+
+    if (curTable == null) {
+        for (i = 0; i < 2; i++) {
+            tableHandle.insertRow(-1).insertCell(0).innerHTML = "new cell";
+        }
+    }
+    else {
+        var cur = JSON.parse(curTable);
+        for (i = 0; i < cur.length; i++) {
+            console.log(cur[i]);
+            tableHandle.insertRow(-1).insertCell(0).innerHTML = cur[i];
+        }
+    }
+}
+
+function cacheChatTable () {
+    var tableHandle = document.getElementById("live_table");
+    var tableArr = new Array();
+    for (i = 0; i < tableHandle.rows.length; i++) {
+        tableArr[tableArr.length] = tableHandle.rows[i].cells[0].innerHTML;
+    }
+    for (i = 0; i < tableArr.length; i++) {
+        console.log(tableArr[i]);
+    }
+    determineTable(whoDis, toWho);
+    localStorage.setItem(message_p1+"/"+message_p2, JSON.stringify(tableArr));
+}
+
+function clearTable() {
+    var tableHandle = document.getElementById("live_table");
+    while (tableHandle.rows.length > 0) {
+        tableHandle.deleteRow(0);
+    }
+    console.log("deletedTable");
+}
+
+function send_message() {
+    messageElement = document.getElementById("chat_input");
+    message = document.getElementById("chat_input").value;
+    add2live(message);
+    document.getElementById("chat_input").value = "";
+}
+
+function add2live(message) {
+    var table = document.getElementById("live_table");
+
+    var newRow = table.insertRow(-1);
+    var cell = newRow.insertCell(0);
+    cell.innerHTML = message;
+    cacheChatTable();
+}
+function setWho(who) {
+    toWho = who;
+    console.log(toWho);
+    clearTable();
+    displayTable();
+}
+function determineTable(p1, p2) {
+    if ((p1 == "client" && p2 == "trainer") || (p1 == "trainer" && p2 == "client")) {
+        message_p1= "client";
+        message_p2= "trainer";
+    }
+    else if ((p1 == "client" && p2 == "doctor") || (p1 == "doctor" && p2 == "client")) {
+        message_p1= "client";
+        message_p2= "doctor";
+    }
+    else if ((p1 == "trainer" && p2 == "doctor") || (p1 == "doctor" && p2 == "trainer")) {
+        message_p1= "trainer";
+        message_p2= "doctor";
+    }
+}
+function setFirstWhos(loc) {
+    loc = String(loc);
+    if (loc.search("client_front_page") > -1) {
+        whoDis = "client";
+        toWho = "trainer";
+    }
+    else if (loc.search("trainer_front_page") > -1) {
+        whoDis = "trainer";
+        toWho = "client";
+    }
+}
