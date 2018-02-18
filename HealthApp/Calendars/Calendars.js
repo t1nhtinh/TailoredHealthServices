7d91b6f200671c2a43648589ed6923f76aa79dcc
smrahman129 <smrahman@ucsd.edu>
2018-02-18 00:03:19 -0800

    Working client search and training calendar removal

--- HealthApp/Calendars/Calendars.js
+++ HealthApp/Calendars/Calendars.js
@@ -7,7 +7,7 @@ function loadTrainingData() {
     var before = "";
     if (localStorage.getItem("training_calendar") != null) {
         toStore = JSON.parse(localStorage.getItem("training_calendar"));
-        console.log(toStore);
+        //console.log(toStore);
         for (i = 0; i < toStore.length; i++) {
             var newTr = document.createElement("tr");
             var newCell = newTr.insertCell(0);
@@ -23,18 +23,25 @@ function loadTrainingData() {
 }
 
 function addListeners() {
-    //var trainer_inputs = document.getElementsByClassName("input_values");
-    ////console.log(trainer_inputs);
-    //for (i = 0; i < trainer_inputs.length; i++) {
-    //    trainer_inputs[i].addEventListener("click", function () {
-    //        var rowIndex = this.parentNode.rowIndex;
-    //        var table = document.getElementById("main_calendar");
-    //        var row = table.rows[rowIndex];
-    //        var index = 0;
-    //        localStorage.setItem("training_calendar", JSON.stringify(toStore));
-    //    });
-    //    trainer_inputs[i].innerHTML = trainer_inputs[i].innerHTML.split("%")[0];
-    //}
+    console.log(toStore);
+    var trainer_inputs = document.getElementsByClassName("input_values");
+    console.log(trainer_inputs);
+    for (i = 0; i < trainer_inputs.length; i++) {
+        trainer_inputs[i].addEventListener("click", function () {
+            var rowIndex = this.parentNode.rowIndex;
+            var table = document.getElementById("main_calendar");
+            var row = table.rows[rowIndex];
+            var index = 0;
+            for (i = 0; i < toStore.length; i++) {
+                if (toStore[i].split("/%/")[0] == row.cells[0].innerHTML) {
+                    index = i;
+                }
+            }
+            toStore.splice(index, 1);
+            table.deleteRow(rowIndex);
+            localStorage.setItem("training_calendar", JSON.stringify(toStore));
+        });
+    }
 }
 
 function createTrainingNode(day,text_in) {
@@ -51,7 +58,7 @@ function createTrainingNode(day,text_in) {
         localStorage.setItem("training_calendar", JSON.stringify(toStore));
     });
     toStore.push(document.getElementById(text_in).value + "/%/"+ day);
-    console.log(toStore);
+    //console.log(toStore);
     newCell.innerHTML = document.getElementById(text_in).value;
     localStorage.setItem("training_calendar", JSON.stringify(toStore));
     before.parentNode.insertBefore(newTr,before);
---
+++ HealthApp/TrainerPage/ClientSearch.js
@@ -0,0 +1,20 @@
+﻿
+function searchTable() {
+    var input;
+    var filter;
+    var tableHandle;
+    input = document.getElementById("client_search");
+    filter = input.value.toUpperCase();
+    tableHandle = document.getElementById("client_list");
+    tableData = tableHandle.getElementsByTagName("td");
+    for( i = 0; i< tableData.length;i++){
+        td = tableData[i].innerHTML;
+        if (td) {
+            if (td.toUpperCase().indexOf(filter) > -1) {
+                tableData[i].style.display = "";
+            } else {
+                tableData[i].style.display = "none";
+            }
+        } 
+    }
+}
